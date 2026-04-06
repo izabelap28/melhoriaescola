0f26117d26a72a311479cefdd8d7b768dd8cda82
izabelap28 <izabela.parizotto@escola.pr.gov.br>
2026-04-06 09:12:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,77 @@
+let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
 
-function comprar(){
+function adicionarCarrinho(nome, preco){
+
+carrinho.push({nome, preco});
+
+localStorage.setItem("carrinho", JSON.stringify(carrinho));
+
+atualizarContador();
+
 alert("Produto adicionado ao carrinho");
-}
-
-function simular(){
-
-let status = document.getElementById("status");
-
-status.innerText = "⚠️ Perigo Detectado";
-status.style.color = "red";
-
-let audio = new Audio("https://www.soundjay.com/button/beep-07.wav");
-
-audio.play();
-
-setTimeout(()=>{
-status.innerText = "Seguro";
-status.style.color = "green";
-},4000);
 
 }
+
+function atualizarContador(){
+
+let contador = document.getElementById("contador");
+
+if(contador){
+contador.innerText = carrinho.length;
+}
+
+}
+
+function carregarCarrinho(){
+
+let lista = document.getElementById("listaCarrinho");
+let total = document.getElementById("total");
+
+if(!lista) return;
+
+lista.innerHTML = "";
+
+let soma = 0;
+
+carrinho.forEach((produto, index)=>{
+
+let item = document.createElement("li");
+
+item.innerHTML = `
+${produto.nome} - R$ ${produto.preco}
+<button onclick="remover(${index})">Remover</button>
+`;
+
+lista.appendChild(item);
+
+soma += produto.preco;
+
+});
+
+total.innerText = soma.toFixed(2);
+
+}
+
+function remover(index){
+
+carrinho.splice(index,1);
+
+localStorage.setItem("carrinho", JSON.stringify(carrinho));
+
+carregarCarrinho();
+atualizarContador();
+
+}
+
+function finalizar(){
+
+alert("Compra finalizada com sucesso!");
+
+localStorage.removeItem("carrinho");
+
+location.reload();
+
+}
+
+atualizarContador();
+carregarCarrinho();
